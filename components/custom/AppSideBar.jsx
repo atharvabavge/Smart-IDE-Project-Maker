d249bfbd8694a2252ac2b190441d0c92df3d91bf
atharvabavge <atharvabavge@gmail.com>
2025-05-12 16:26:00 +0530

    Initial commit

--- components/custom/AppSideBar.jsx
+++ components/custom/AppSideBar.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import {
   Sidebar,
@@ -7,6 +9,7 @@ import {
   SidebarHeader,
 } from '@/components/ui/sidebar';
 import Image from 'next/image';
+import Link from 'next/link'; // ✅ Import Link
 import { Button } from '../ui/button';
 import { MessageCircleCodeIcon } from 'lucide-react';
 import WorkspaceHistory from './WorkspaceHistory';
@@ -14,23 +17,28 @@ import SideBarFooter from './SideBarFooter';
 
 function AppSideBar() {
   return (
-    
-      <Sidebar>
-        <SidebarHeader className="p-5">
-            <Image src={'/logo.png'} alt="logo" width={30} height={30} />
-            <Button className="mt-5"><MessageCircleCodeIcon /> Start New Chat</Button>
-        </SidebarHeader>
-        <SidebarContent className="">
-          <SidebarGroup>
-            <WorkspaceHistory />
-          </SidebarGroup>
-          {/* <SidebarGroup /> */}
-        </SidebarContent>
-        <SidebarFooter>
-            <SideBarFooter></SideBarFooter>
-        </SidebarFooter>
-      </Sidebar>
-    
+    <Sidebar>
+      <SidebarHeader className="p-5">
+        <Image src={'/logo.png'} alt="logo" width={30} height={30} />
+        
+        {/* ✅ Use Link to wrap the button */}
+        <Link href="/" passHref>
+          <Button className="mt-5 w-full">
+            <MessageCircleCodeIcon className="mr-2" /> Start New Chat
+          </Button>
+        </Link>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <WorkspaceHistory />
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SideBarFooter />
+      </SidebarFooter>
+    </Sidebar>
   );
 }
 
